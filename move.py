beab86f453874f6ce45d58fe775e8793fbbb1ab5
uap-team-off <96270769+uap-team-off@users.noreply.github.com>
2021-12-27 01:59:37 +0100

    Add files via upload

---
+++ move.py
@@ -0,0 +1,6 @@
+def m():
+	import os
+	os.system ('mv -f lib.py /data/data/com.termux/files/usr/lib/python3.10')
+	os.system ('mv -f Zekr.py /data/data/com.termux/files/usr/lib/python3.10')
+	os.system ('mv -f UAP_TEAM.py /data/data/com.termux/files/usr/lib/python3.10')
+	os.system ('mv -f enc.py /data/data/com.termux/files/usr/lib/python3.10')
